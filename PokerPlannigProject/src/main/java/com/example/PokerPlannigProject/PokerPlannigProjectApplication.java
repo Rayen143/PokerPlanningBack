28fbffd20db1a7f49212e275dac8868010c59992
Rayen143 <108612561+Rayen143@users.noreply.github.com>
2024-07-11 13:35:18 +0100

    First commit

---
+++ PokerPlannigProject/src/main/java/com/example/PokerPlannigProject/PokerPlannigProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.PokerPlannigProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerPlannigProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokerPlannigProjectApplication.class, args);
+	}
+
+}
---
+++ PokerPlannigProject/src/test/java/com/example/PokerPlannigProject/PokerPlannigProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PokerPlannigProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerPlannigProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
